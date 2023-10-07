f6155a19f7607fcd7bf6613e17bf0efe8b77ebc8
vonvolous <hanyoojin39@gmail.com>
2023-10-08 00:09:59 +0900

    ajax 이메일 중복 체크

--- src/main/java/com/springboot/member/controller/MemberController.java
+++ src/main/java/com/springboot/member/controller/MemberController.java
@@ -5,10 +5,7 @@ import com.springboot.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -90,5 +87,17 @@ public class MemberController {
     public String logout(HttpSession session) {
         session.invalidate();
         return "index";
+    }
+
+    @PostMapping("/member/email-check")
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail) {
+        System.out.println("memberEmail = " + memberEmail);
+        String checkResult = memberService.emailCheck(memberEmail);
+        return checkResult;
+        /*if (checkResult != null) {
+            return "ok";
+        } else {
+            return "no";
+        }*/
     }
 }
