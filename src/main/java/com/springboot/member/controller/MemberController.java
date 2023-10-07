b591fd22b7a5f1ffeecf41b513cd931d63228448
vonvolous <hanyoojin39@gmail.com>
2023-10-07 20:49:16 +0900

    login 기능 구현

--- src/main/java/com/springboot/member/controller/MemberController.java
+++ src/main/java/com/springboot/member/controller/MemberController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -26,5 +28,23 @@ public class MemberController {
         System.out.println("memberDTO = " + memberDTO);
         memberService.save(memberDTO);
         return "login";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if(loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        } else {
+            // login 실패
+            return "login";
+        }
     }
 }
--- src/main/java/com/springboot/member/repository/MemberRepository.java
+++ src/main/java/com/springboot/member/repository/MemberRepository.java
@@ -4,5 +4,10 @@ package com.springboot.member.repository;
 import com.springboot.member.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    // 이메일로 회원 정보 조회 (select * from member_table where member_email=?)
+    // Optional은 일종의 null 방지를 해준다.
+    Optional<MemberEntity> findByMemberEmail(String memberEmail);
 }
